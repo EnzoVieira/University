8c3d6fca423ec702079a7c27b2a5340e0bb1ff82
Enzo Vieira <enzovieira16@hotmail.com>
2021-11-09 17:17:51 +0000

    Fazendo até a questão 32 das 50 questões

--- ProgramacaoFuncional/50Questoes/50Questoes.hs
+++ ProgramacaoFuncional/50Questoes/50Questoes.hs
@@ -162,7 +162,6 @@ idadeRecursive year age ((name, dob) : xs)
   | otherwise = idadeRecursive year age xs
 
 -- 20.
--- 1 4
 powerEnumFrom :: Int -> Int -> [Int]
 powerEnumFrom _ 1 = [1]
 powerEnumFrom base power =
@@ -170,3 +169,157 @@ powerEnumFrom base power =
    in if power <= 0
         then []
         else r ++ [base ^ (power - 1)]
+
+-- 21.
+isPrime :: Int -> Bool
+isPrime n = isPrimeAuxiliar n (n - 1)
+
+-- isPrime n = null [x | x <- [2 .. n -1], n `mod` x == 0]
+
+isPrimeAuxiliar :: Int -> Int -> Bool
+isPrimeAuxiliar n 2 = True
+isPrimeAuxiliar n m
+  | mod n m == 0 = False
+  | otherwise = isPrimeAuxiliar n (m - 1)
+
+-- 22.
+isPrefixOfTeste1 :: [Int]
+isPrefixOfTeste1 = [10, 20]
+
+isPrefixOfTeste2 :: [Int]
+isPrefixOfTeste2 = [10, 20, 30]
+
+isPrefixOfTeste3 :: [Int]
+isPrefixOfTeste3 = [10, 30]
+
+isPrefixOf :: Eq a => [a] -> [a] -> Bool
+isPrefixOf (x : xs) (y : ys) = x == y && isPrefixOf xs ys
+isPrefixOf (x : xs) [] = False
+isPrefixOf _ _ = True
+
+-- 23.
+isSuffixOfTeste1 :: [Int]
+isSuffixOfTeste1 = [20, 30]
+
+isSuffixOfTeste2 :: [Int]
+isSuffixOfTeste2 = [10, 20, 30]
+
+isSuffixOf :: Eq a => [a] -> [a] -> Bool
+isSuffixOf [] _ = True
+isSuffixOf _ [] = False
+isSuffixOf l1 l2 = last l1 == last l2 && isSuffixOf (init l1) (init l2)
+
+-- 24.
+isSubsequenceOfTeste1 :: [Int]
+isSubsequenceOfTeste1 = [40, 20]
+
+isSubsequenceOfTeste2 :: [Int]
+isSubsequenceOfTeste2 = [10, 20, 30, 40]
+
+isSubsequenceOf :: Eq a => [a] -> [a] -> Bool
+isSubsequenceOf [] _ = True
+isSubsequenceOf _ [] = False
+isSubsequenceOf (x : xs) (y : ys) =
+  if x == y
+    then isSubsequenceOf xs ys
+    else isSubsequenceOf (x : xs) ys
+
+-- 25.
+elemIndicesTeste :: [Int]
+elemIndicesTeste = [1, 2, 3, 4, 3, 2, 3, 4, 5]
+
+elemIndices :: Eq a => a -> [a] -> [Int]
+elemIndices _ [] = []
+elemIndices el l = elemIndicesAux el l 0
+
+elemIndicesAux :: Eq a => a -> [a] -> Int -> [Int]
+elemIndicesAux _ [] _ = []
+elemIndicesAux el (x : xs) index =
+  if el == x
+    then index : elemIndicesAux el xs (index + 1)
+    else elemIndicesAux el xs (index + 1)
+
+-- 26. REQUER REVISAO
+nubRecursiveTeste :: [Int]
+nubRecursiveTeste = [1, 2, 1, 2, 3, 1, 2]
+
+nubRecursive :: Eq a => [a] -> [a]
+nubRecursive [] = []
+nubRecursive (x : xs) =
+  let r = nubRecursive xs
+   in if x `elem` r
+        then r
+        else x : r
+
+-- 27.
+deleteRecursiveTeste :: [Int]
+deleteRecursiveTeste = [1, 2, 1, 2, 3, 1, 2]
+
+deleteRecursive :: Eq a => a -> [a] -> [a]
+deleteRecursive _ [] = []
+deleteRecursive el (x : xs)
+  | x == el = xs
+  | otherwise = x : deleteRecursive el xs
+
+-- 28.
+deleteFirstsRecursiveTeste1 :: [Int]
+deleteFirstsRecursiveTeste1 = [1, 2, 3, 4, 5, 1]
+
+deleteFirstsRecursiveTeste2 :: [Int]
+deleteFirstsRecursiveTeste2 = [1, 5, 4]
+
+deleteFirstsRecursive :: Eq a => [a] -> [a] -> [a]
+deleteFirstsRecursive l1 [] = l1
+deleteFirstsRecursive [] _ = []
+deleteFirstsRecursive (x : xs) l2 =
+  if x `elem` l2
+    then deleteFirstsRecursive xs (deleteFirstsRecursiveDropElem x l2)
+    else x : deleteFirstsRecursive xs l2
+
+deleteFirstsRecursiveDropElem :: Eq a => a -> [a] -> [a]
+deleteFirstsRecursiveDropElem _ [] = []
+deleteFirstsRecursiveDropElem el (x : xs)
+  | el == x = xs
+  | otherwise = x : deleteFirstsRecursiveDropElem el xs
+
+-- 29.
+unionRecursive :: Eq a => [a] -> [a] -> [a]
+unionRecursive l1 (y : ys) =
+  if y `elem` l1
+    then unionRecursive l1 ys
+    else l1 ++ [y]
+unionRecursive _ _ = []
+
+-- 30.
+intersectRecursiveTeste1 :: [Int]
+intersectRecursiveTeste1 = [1, 1, 2, 3, 4]
+
+intersectRecursiveTeste2 :: [Int]
+intersectRecursiveTeste2 = [1, 3, 5]
+
+intersectRecursive :: Eq a => [a] -> [a] -> [a]
+intersectRecursive (x : xs) l2 =
+  if x `elem` l2
+    then x : intersectRecursive xs l2
+    else intersectRecursive xs l2
+intersectRecursive _ _ = []
+
+-- 31.
+insertRecursiveTeste :: [Int]
+insertRecursiveTeste = [1, 20, 30, 40]
+
+insertRecursive :: Ord a => a -> [a] -> [a]
+insertRecursive _ [] = []
+insertRecursive el (x : xs) =
+  if x > el
+    then el : x : xs
+    else x : insertRecursive el xs
+
+-- 32.
+unwordsRecursiveTeste :: [String]
+unwordsRecursiveTeste = ["Programacao", "Funcional"]
+
+unwordsRecursive :: [String] -> String
+unwordsRecursive [] = []
+unwordsRecursive [x] = x
+unwordsRecursive (x : xs) = x ++ " " ++ unwordsRecursive xs
