97c749d466bddada6978fa2fde5531193afec6d3
Enzo Vieira <enzovieira16@hotmail.com>
2022-02-20 16:25:44 +0000

    add(w3schools): Adding myfirstprogram

--- ProgramacaoImperativa/w3schools/myfirstprogram.c
+++ ProgramacaoImperativa/w3schools/myfirstprogram.c
@@ -19,5 +19,26 @@ int main() {
   printf("%c\n", charVariableName);
 
   const int MYCONSTINT = 15;
+
+  // Operadores de comparação devolvem
+  // ou 1 (true) ou 0 (false)
+  printf("%d\n", 1 > 3);
+
+
+  // The memory size (in bytes) of a data type 
+  // or a variable can be found with the sizeof operator:
+  int myInt;
+  float myFloat;
+  double myDouble;
+  char myChar;
+
+  // Note that we use the %lu format specifer to print the result, 
+  // instead of %d. It is because the compiler expects the sizeof 
+  // operator to return a long unsigned int (%lu), instead of int (%d). 
+  // On some computers it might work with %d, but it is safer to use %lu.
+  printf("%lu\n", sizeof(myInt));
+  printf("%lu\n", sizeof(myFloat));
+  printf("%lu\n", sizeof(myDouble));
+  printf("%lu\n", sizeof(myChar));
   return 0;
 }
